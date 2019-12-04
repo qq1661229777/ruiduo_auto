6ae99629cd5e45a9907cb00a34aa2b98a01d1918
qq1661229777 <1661229777@qq.com>
2019-12-04 11:59:28 +0800

    https://github.com/qq1661229777/-.git

--- pageElement/setPage.py
+++ pageElement/setPage.py
@@ -1,7 +1,7 @@
 '''
 @Author: your name
 @Date: 2019-12-04 10:48:26
-@LastEditTime: 2019-12-04 11:55:03
+@LastEditTime: 2019-12-04 11:57:11
 @LastEditors: Please set LastEditors
 @Description: In User Settings Edit
 @FilePath: \REEAD_UI\pageElement\setPage.py
@@ -30,7 +30,7 @@ def click_year(driver):
 # 个人信息-生日-选择月 xpath
 def click_month(driver):
     driver.find_element_by_xpath('//*[@resource-id="android:id/content"]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.LinearLayout[1]/android.view.View[2]').click()
-# 个人信息-生日-选择日 xpath
+# 个人信息-生日-选择日 xpat
 def click_tv_day(driver):
     driver.find_element_by_xpath('//*[@resource-id="android:id/content"]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.LinearLayout[1]/android.view.View[3]').click()
 # 个人信息-生日-选择确认 xpath
